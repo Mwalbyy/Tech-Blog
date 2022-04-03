836db7e4300cdeb83ce650410e1511fa402555b3
Marcus Walby <marcuswalby24@gmail.com>
2022-04-03 13:58:21 -0400

    finished comment and user routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -2,13 +2,11 @@ const router = require('express').Router();
 const { Comment } = require('../../models/');
 const withAuth = require('../../utils/auth');
 
-// URL: /api/comment
 router.post('/', withAuth, async (req, res) => {
   try {
     const newComment = await Comment.create({
-      // TODO: COMMENT BODY IN REQUEST USING SPREAD
-
-      // TODO: SET USERID userId TO SESSION LOGGEDIN USERID
+      ...req.body,
+      userId: req.session.userId
     });
     res.json(newComment);
   } catch (err) {
